322cc3b1b98f7db03a89f51fa20972d963da13e1
Chad Smith <chadsmith71@hotmail.com>
2024-01-17 15:02:24 -0600

    Add files via upload
    Initial commit

---
+++ 1-intro.js
@@ -0,0 +1,11 @@
+// Node.js tutorial by freeDCodeCamp.org at https://www.youtube.com/watch?v=Oe421EPjeBE 
+
+const amount = 9;
+
+if (amount < 10) {
+  console.log('small number');
+} else {
+  console.log('large number');
+}
+
+console.log(`hey it's my first node app!!!`);
